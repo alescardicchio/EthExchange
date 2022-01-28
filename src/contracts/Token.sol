48e4e653c6c973ffd9fd05620480e3b55618295b
alexandrurotariu <alex.rotariu.1998@gmail.com>
2022-01-28 16:50:36 +0100

    Implementata funzione per trasferimento token

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,16 +1,49 @@
 pragma solidity >=0.4.20;
 
+//il nostro token
 contract Token {
 
-    //costruttore
-
+    //nome token
+    string public name = "RomaTre Token";
+    //simbolo token
+    string public symbol = "RM3";
+    //definizione dallo standard
     uint256 public totalSupply;
+    //mappa indirizzi -> saldo token 
+    mapping(address => uint256) public balanceOf;
 
-    constructor() public {
-        totalSupply = 1000000;
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
+    //costruttore 
+    constructor(uint256 _initialSupply) public {
+        //msg.sender inidrizzo chi effettua il deploy del contratto
+        //assegnazione dei token ad un indirizzo
+        balanceOf[msg.sender] = _initialSupply;
+        //set numero totale di tokens
+        totalSupply = _initialSupply;
     }
-    //set numero totale di tokens
 
-    //legge il numero totale di tokens
+    //trasferimento (return un boolano)
+    //eccezione se l'account non ha saldo sufficiente
+    function transfer(address _to, uint256 _value) public returns (bool success){
+        //require -> se la condizione è vera continua l'esecuzione, altrimenti errore
+        require(balanceOf[msg.sender] >= _value);
+        //trasferiento della somma
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        //transfer event
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+
+    
+    //funzione getter creata di default dallo standard
+    //funzione balanceOf creata di default dallo standard
     
 }
--- test/TokenTest.js
+++ test/TokenTest.js
@@ -5,12 +5,55 @@ var Token = artifacts.require("Token");
 
 contract('Token', function(accounts){
     
-    it('set token totali al deployment', function() {
+    var tokenInstance;
+
+    it('inizializza il contratto con i valori corretti', function() {
+        return Token.deployed().then(function(instance){
+            tokenInstance = instance;
+            return tokenInstance.name();
+        }).then(function(name){
+            assert.equal(name, 'RomaTre Token', 'il nome è corretto');
+            return tokenInstance.symbol();
+        }).then(function(symbol){
+            assert.equal(symbol, 'RM3', 'il simbolo è corretto');
+        });
+    });
+
+    it('set token iniziali al deployment', function() {
         return Token.deployed().then(function(instance){
             tokenInstance = instance;
             return tokenInstance.totalSupply();
         }).then(function(totalSupply){
             assert.equal(totalSupply.toNumber(), 1000000, 'set token totali a 1,000,000');
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(adminBalance){
+            assert.equal(adminBalance.toNumber(),1000000, 'assegnazione token all admin');
+        });
+    });
+
+    
+    it('trasferimento token', function() {
+        return Token.deployed().then(function(instance){
+            tokenInstance = instance;
+            return tokenInstance.transfer.call(accounts[1], 99999999999);
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf('revert') >=0, 'errore saldo insufficiente');
+            return tokenInstance.transfer.call(accounts[1], 250000, {from: accounts[0]});
+        }).then(function(success){
+            assert.equal(success, true, 'torna true');
+            return tokenInstance.transfer(accounts[1], 250000, {from: accounts[0]});
+        }).then(function(receipt){
+            assert.equal(receipt.logs.length, 1, 'triggera un evento');
+            assert.equal(receipt.logs[0].event, 'Transfer', 'dovrebbe essere evento "Transfer"');
+            assert.equal(receipt.logs[0].args._from, accounts[0], 'log da cui vengono trasferiti i token');
+            assert.equal(receipt.logs[0].args._to, accounts[1], 'log verso cui vengono trasferiti i token');
+            assert.equal(receipt.logs[0].args._value, 250000, 'log l ammontare di token trasferiti');
+            return tokenInstance.balanceOf(accounts[1]);
+        }).then(function(balance){
+            assert.equal(balance.toNumber(), 250000, 'token aggiunti all account destinatario');
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(balance){
+            assert.equal(balance.toNumber(), 750000, 'token sottratti all account mittente');
         });
     });
 })
